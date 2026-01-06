cdce3246ebe2b146b1993f1ef129fe82aadc8b98
Inzamul Sharafot <enzymesharafot@gmail.com>
2026-01-06 20:33:06 +0600

    fix: Explicitly install setuptools in build script

--- backend/build.sh
+++ backend/build.sh
@@ -3,6 +3,9 @@
 
 set -o errexit
 
+# Install setuptools first to provide pkg_resources
+pip install --upgrade pip setuptools wheel
+
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
